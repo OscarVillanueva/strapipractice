fb65325e4dc0ca6d3839ea9725f72bed8bde6c45
Oscar Villanueva <javiondo68@gmail.com>
2021-02-08 13:35:41 -0600

    Initial commi

---
+++ config/database.js
@@ -0,0 +1,18 @@
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'mysql',
+        host: env('DATABASE_HOST', '192.168.1.82'),
+        port: env.int('DATABASE_PORT', 3306),
+        database: env('DATABASE_NAME', 'bienesraices'),
+        username: env('DATABASE_USERNAME', 'dev1'),
+        password: env('DATABASE_PASSWORD', 'dev1'),
+        ssl: env.bool('DATABASE_SSL', false),
+      },
+      options: {}
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'fdf7d5f0-a03e-4dad-b5e3-05a45b396288'
+};
